9157dc0ba15d2fbef66e6fc8a6347d21ce6dec21
dkdud8140 <cho.ay4@gmail.com>
2021-03-16 15:14:39 +0900

    Hello_02 삭제

--- Java_00_Hello/src/com/callor/hello/Hello_02.java
+++ Java_00_Hello/src/com/callor/hello/Hello_02.java
@@ -4,10 +4,7 @@ public class Hello_02 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("반갑습니다");
-		System.out.println(30+50);
-		System.out.println("하ㅣ하하하");
-		System.out.println(60*0.5);
+		
 	}
 	
 }
